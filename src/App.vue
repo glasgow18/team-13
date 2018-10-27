378cc11f339edae74d2266c2077122268261f30c
Gregor Beatson <gregor.beatson@gmail.com>
2018-10-27 20:40:31 +0100

    chat input component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <message msg="hi"/>
+
+    <chat-input/>
   </div>
 </template>
 
 <script>
 import Message from './components/Message.vue'
+import ChatInput from './components/ChatInput.vue'
 
 export default {
   name: 'app',
   components: {
-    Message
+    Message,
+    ChatInput
   }
 }
 </script>
---
+++ src/components/ChatInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="sendMsg">
+    <div class="chatinput input-group">
+      <input v-model="currentMsg" type="text" class="form-control" placeholder="Message" aria-describedby="button-addon2" autofocus>
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Send</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ChatInput',
+  methods: {
+    sendMsg: function () {
+      this.currentMsg=""
+    }
+  },
+  data() {
+    return {
+      currentMsg: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chatinput {
+  position: fixed;
+  bottom: 0;
+  padding: 1px;
+}
+
+.form-control:focus, .btn:focus {
+  box-shadow: none;
+  outline: none;
+}
+</style>
