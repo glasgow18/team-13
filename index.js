6a061bfb5f371094f28a1b768e2c6d605b268729
Gregor Beatson <gregor.beatson@gmail.com>
2018-10-27 17:32:03 +0100

    Message component defined

---
+++ index.js
@@ -0,0 +1,7 @@
+Vue.component('message', {
+  template: '<div style="border:1px solid #000">this is a nice message</div>'
+})
+
+var app = new Vue({
+  el: '#app'
+})
---
+++ messge.js
@@ -0,0 +1,3 @@
+Vue.component('message', {
+  template: '<div>Nice message</div>'
+})
